57a9ae9e276c60feaacfebca8048c3d2b1fbeee2
andyt <andreas.b.teixeira@gmail.com>
2019-12-01 17:59:30 +0100

    Coockie implementierung
    Cookie werden über Skript auf Webseite implementiert, Ausserdem wird eine responsive web (screensice eingefügt))

---
+++ script.js
@@ -0,0 +1,28 @@
+Cookies.set('datelog', 'true', { expires: 7 , domain: 'jojowanuby.github.io/Test/' });
+
+var myCookie = Cookies.get('datelog');
+
+window.onload = function() {
+localStorage.setItem("Date"+n ,datetime)
+}
+
+
+//auch wenn man in console myCookie eingiebt leider value als undefined!!!!!
+console.log(myCookie);
+console.log(localStorage)
+
+//Cookie Zeigt leider value als undefined!!!!!
+Cookies.set('datetoday', datetime, { expires: 7 },{ domain: 'jojowanuby.github.io/Test/' });
+
+
+// For todays date;
+Date.prototype.today = function () { 
+  return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+}
+
+// For the time now
+Date.prototype.timeNow = function () {
+   return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+}
+
+var datetime = "LastSync: " + new Date().today() + " @ " + new Date().timeNow();
