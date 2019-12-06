ac5ee69828dfb2357a9f4c1e140c9b48b08e250d
andyt <andreas.b.teixeira@gmail.com>
2019-12-06 11:41:21 +0100

    navi-1

--- script.js
+++ script.js
@@ -32,3 +32,30 @@ Date.prototype.timeNow = function () {
 }
 
 var datetime = "LastSync: " + new Date().today() + " @ " + new Date().timeNow();
+
+
+
+
+// NEW Try (Navigationbar)
+
+// Sticky Header
+$(window).scroll(function() {
+
+  if ($(window).scrollTop() > 100) {
+      $('.main_h').addClass('sticky');
+  } else {
+      $('.main_h').removeClass('sticky');
+  }
+});
+
+
+// navigation scroll lijepo radi materem
+$('nav a').click(function(event) {
+  var id = $(this).attr("href");
+  var offset = 70;
+  var target = $(id).offset().top - offset;
+  $('html, body').animate({
+      scrollTop: target
+  }, 500);
+  event.preventDefault();
+});
